a40ecc86423c28780ec6a65daf3e95e3258ecd21
wanxiaolong <wanxiaolong100@163.com>
2017-12-03 17:01:16 +0800

    add i18n resources

--- src/main/java/com/my/evc/controller/springmvc/ServerController.java
+++ src/main/java/com/my/evc/controller/springmvc/ServerController.java
@@ -1,30 +1,55 @@
 package com.my.evc.controller.springmvc;
 
+import java.util.Locale;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.LocaleResolver;
 
 import com.my.evc.common.JsonResponse;
 import com.my.evc.exception.BaseException;
 
+/**
+ * 和业务没有直接关系的请求。
+ */
 @Controller
 @RequestMapping("/server")
 public class ServerController extends BaseController {
     private static final String JSON_TYPE = "application/json";
-    
+
+    @Autowired
+    private LocaleResolver localeResolver;
+
     /**
-     * Self service to check whether this service is up or down.
+     * 自检。测试当前server是否能正常响应。
      */
     @ResponseBody
-    @RequestMapping(value="/ping", method = RequestMethod.GET, produces=JSON_TYPE)
-    public JsonResponse<String> ping(String name,
-            HttpServletRequest request, HttpServletResponse response)
+    @RequestMapping(value = "/ping", method = RequestMethod.GET, produces = JSON_TYPE)
+    public JsonResponse<String> ping(String name, HttpServletRequest request, HttpServletResponse response)
             throws BaseException, Exception {
         System.out.println("============" + name + "==================");
         return new JsonResponse<String>(SUCCESS, name);
+    }
+
+    /**
+     * 语言切换。
+     */
+    @RequestMapping(value = "lang", method = RequestMethod.GET)
+    public String language(HttpServletRequest request, HttpServletResponse response, 
+            @RequestParam("lang") String language) {
+        language = language.toLowerCase();
+        if ("en".equalsIgnoreCase(language)) {
+            localeResolver.setLocale(request, response, Locale.US);
+        } else {
+            localeResolver.setLocale(request, response, Locale.CHINA);
+        }
+        return "index";
     }
 }
--- src/main/webapp/views/index.jsp
+++ src/main/webapp/views/index.jsp
@@ -1,8 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<%@ taglib prefix="i18n" uri="http://www.springframework.org/tags" %>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Bootstrap页面模板</title>
+	<title><i18n:message code="welcome"/></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="vendor/bootstrap-3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" href="vendor/font-awesome-4.7.0/css/font-awesome.min.css">
@@ -10,6 +11,9 @@
 	<script src="vendor/bootstrap-3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<h1>Hello World!</h1>
+	<h1><i18n:message code="hello"/></h1>
+	<h1><i18n:message code="key"/></h1>
+	<a href="/evc/rest/server/lang?lang=zh">中文</a>
+	<a href="/evc/rest/server/lang?lang=en">English</a>
 </body>
 </html>
